b2c9e52b413a92f16ec3dca5ed0de54ae6ffed51
Sonia R <shimanlia0234@gmail.com>
2025-06-27 03:34:50 +0000

    Raw table code changes

--- models/raw/country_details_raw.sql
+++ models/raw/country_details_raw.sql
@@ -1,3 +1,16 @@
+
+/* 
+Configarations are nothing but , those are the values for  that particular DBT Model.
+"materialized":'table', --> We ewant to create this  particular code asa table inside  our snowflake. Thats why we pass materilization inside our table.
+"pre_hook": copy_json('COUNTRY_DETAILS_CP'), --> Passing a pre hook statement means any statement you wanna  run before the  below  "with cte statement" , you need to pass it to pre hook. So prehook statement run first and then the selow select statement run.
+copy_json('COUNTRY_DETAILS_CP') --> copy_jason is the macro we created in previous "copy into sql file"
+
+"schema": 'RAW'
+
+*/
+
+
+
 {{
 
 config
